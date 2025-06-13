a850356fe48e26d263e7a637e965bf573850dd04
tdayris <thibault.dayris@gustaveroussy.fr>
2025-06-13 14:05:26 +0200

    add deeptools value if available

--- workflow/scripts/append_agat_statistics.py
+++ workflow/scripts/append_agat_statistics.py
@@ -54,11 +54,26 @@ def load_genome_size(path: str) -> str:
 
 agat = load_yaml(snakemake.input.agat)
 agat = fmt_keys(agat)
-agat["genome_size"] = load_genome_size(snakemake.input.gs)
+agat["genome_size"] = {"computed": load_genome_size(snakemake.input.gs)}
+
+deeptools_genome_size = fmt_keys(
+    {
+        "GRCh37": 2_864_785_220,
+        "GRCh38": 2_913_022_398,
+        "T2T/CHM13CAT_v2": 3_117_292_070,
+        "GRCm37": 2_620_345_972,
+        "GRCm38": 2_652_783_500,
+        "GRCm39": 2_654_621_783,
+        "dm3": 162_367_812,
+        "dm6": 142_573_017,
+        "GRCz10": 1_369_631_918,
+        "GRCz11": 1_368_780_147,
+        "WBcel235": 100_286_401,
+        "TAIR10": 119_482_012,
+    }
+)
+build = str(snakemake.wildcards.build).lower()
+agat["genome_size"]["theoretical"] = deeptools_genome_size.get(build, None)
 
 with open(snakemake.output.yaml, "w") as yaml_stream:
-    yaml_stream.write(
-        yaml.dump(
-            agat,
-        )
-    )
+    yaml_stream.write(yaml.dump(agat))
